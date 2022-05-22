c02beab5ec9e43b5f45507766596846486baf0dc
Viktorhjh <Viktorhjh@VIKTORHJH>
2022-05-22 17:49:30 +0200

    Add project files.

---
+++ ZPO_lab9(2)/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace ZPO_lab9_2_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.listLinks = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(402, 187);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Download";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // listLinks
+            // 
+            this.listLinks.FormattingEnabled = true;
+            this.listLinks.Items.AddRange(new object[] {
+            "https://www.google.com/",
+            "https://www.microsoft.com/"});
+            this.listLinks.Location = new System.Drawing.Point(12, 10);
+            this.listLinks.Name = "listLinks";
+            this.listLinks.Size = new System.Drawing.Size(384, 488);
+            this.listLinks.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(402, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add new ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(402, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(402, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Clear ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 513);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listLinks);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox listLinks;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ZPO_lab9(2)/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZPO_lab9_2_
+{
+    public partial class Form1 : Form
+    {
+        List<Task<string>> taskList = new List<Task<string>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }     
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listLinks.Items.Add(textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listLinks.Items.Clear();
+        }
+
+        //Download
+        private async void Button2_Click(object sender, EventArgs e)
+        {
+            Parallel.For(0, listLinks.Items.Count, i =>
+            {
+                taskList.Add(download(listLinks.Items[i].ToString()));
+            });                        
+
+            foreach(Task<string> task in taskList)
+            {
+                MessageBox.Show(task.Result);
+                
+            }
+            
+        }
+
+        public async Task<string> download(string link)
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                string res = await client.GetStringAsync(link);
+                return res;
+            }
+        }
+    }
+}
